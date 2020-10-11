531567c004170a754b1b53a33c3b4bbd655176d2
Tobias Newe <tobias.newe@t-online.de>
2020-10-11 20:12:41 +0200

    camera: added basic functionality

--- inc/camera.hpp
+++ inc/camera.hpp
@@ -6,10 +6,21 @@ class Camera : public Object3D {
     public:
         Camera(float fov, float aspect, float near = 0.1f, float far = 1000.0f);
 
-        void set_aspect(float aspect);
-
         glm::mat4 view_matrix() const;
         glm::mat4 proj_matrix() const;
+
+        glm::vec3 front() const;
+        glm::vec3 right() const;
+        glm::vec3 up() const;
+
+        void move_forward(float value);
+        void move_backward(float value);
+        void move_right(float value);
+        void move_left(float value);
+        void move_up(float value);
+        void move_down(float value);
+
+        void set_aspect(float aspect);
     private:
         float m_fov;
         float m_aspect;
--- src/camera.cpp
+++ src/camera.cpp
@@ -8,14 +8,42 @@
 Camera::Camera(float fov, float aspect, float near, float far) : m_fov(fov), m_aspect(aspect), m_near(near), m_far(far) {
 }
 
-void Camera::set_aspect(float aspect) {
-    m_aspect = aspect;
-}
-
 glm::mat4 Camera::view_matrix() const {
     return glm::affineInverse(this->model_matrix());
 }
-
 glm::mat4 Camera::proj_matrix() const {
     return glm::perspective(glm::radians(m_fov), m_aspect, m_near, m_far);
+}
+
+glm::vec3 Camera::front() const {
+    return glm::vec3(0.0f, 0.0f, -1.0f) * this->orientation();
+}
+glm::vec3 Camera::right() const {
+    return glm::vec3(1.0f, 0.0f, 0.0f) * this->orientation();
+}
+glm::vec3 Camera::up() const {
+    return glm::vec3(0.0f, 1.0f, 0.0f) * this->orientation();
+}
+
+void Camera::move_forward(float value) {
+    this->translate(value * this->front());
+}
+void Camera::move_backward(float value) {
+    this->move_forward(-value);
+}
+void Camera::move_right(float value) {
+    this->translate(value * this->right());
+}
+void Camera::move_left(float value) {
+    this->move_right(-value);
+}
+void Camera::move_up(float value) {
+    this->translate(value * this->up());
+}
+void Camera::move_down(float value) {
+    this->move_up(-value);
+}
+
+void Camera::set_aspect(float aspect) {
+    m_aspect = aspect;
 }
